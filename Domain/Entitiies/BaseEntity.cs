e91618a8cc6d0cc6cec53ddb524a274075e64df4
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-12-11 21:06:57 -0400

    feat: create base entity

---
+++ Domain/Entitiies/BaseEntity.cs
@@ -0,0 +1,8 @@
+namespace Trackit.Domain.Entity;
+
+public class BaseEntity
+{
+  public Guid Id { get; init; } = Guid.NewGuid();
+  public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+  public DateTime? UpdatedAt { get; set; }
+}
